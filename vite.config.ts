1f96747925e5ee070068685629d97a9223850c5c
byeonseok49-ux <byeonseok49@gmail.com>
2026-01-29 22:41:14 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: '/PharmStock-Pro/', // 이 줄을 추가해야 깃허브에서 정상 작동합니다!
+    server: {
         port: 3000,
         host: '0.0.0.0',
       },
